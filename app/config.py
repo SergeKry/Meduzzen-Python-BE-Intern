315d7815045445ea716857b92beb5ad2759771d6
SergeKryv <krivosheevsrg@gmail.com>
2024-04-15 18:52:31 +0300

    using settings dict, changed docker compose to use PGPORT env variable

--- app/config.py
+++ app/config.py
@@ -1,21 +1,37 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# class Settings(BaseSettings):
+#     uvicorn_host: str = Field(..., env='UVICORN_HOST')
+#     uvicorn_port: int = Field(..., env='UVICORN_PORT')
+#     uvicorn_restart: bool = Field(..., env='UVICORN_RESTART')
+#     db_name: str = Field(..., env='DB_NAME')
+#     db_user: str = Field(..., env='DB_USER')
+#     db_password: str = Field(..., env='DB_PASSWORD')
+#     db_host: str = Field(..., env='DB_HOST')
+#     db_port: int = Field(..., env='DB_PORT')
+#     redis_host: str = Field(..., env='REDIS_HOST')
+#     redis_port: int = Field(..., env='REDIS_PORT')
+
 
 class Settings(BaseSettings):
-    uvicorn_host: str = Field(..., env='UVICONN_HOST')
-    uvicorn_port: int = Field(..., env='UVICONN_PORT')
-    uvicorn_restart: bool = Field(..., env='UVICONN_RESTART')
-    db_name: str = Field(..., env='DB_NAME')
-    db_user: str = Field(..., env='DB_USER')
-    db_password: str = Field(..., env='DB_PASSWORD')
-    db_host: str = Field(..., env='DB_HOST')
-    db_port: int = Field(..., env='DB_PORT')
-    redis_host: str = Field(..., env='REDIS_HOST')
-    redis_port: int = Field(..., env='REDIS_PORT')
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
+    UVICORN_HOST: str
+    UVICORN_PORT: int
+    UVICORN_RESTART: bool
+
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
+    REDIS_HOST: str
+    REDIS_PORT: int
 
 
 settings = Settings()
--- app/db/database.py
+++ app/db/database.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from app.config import settings
 import redis.asyncio as redis
 
-SQLALCHEMY_DATABASE_URL = f'postgresql+asyncpg://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 Base = declarative_base()
 
-async_session = sessionmaker(engine, class_=AsyncSession,expire_on_commit=False)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 # Dependency for the future
@@ -18,7 +18,7 @@ async def get_session() -> AsyncSession:
 
 
 async def init_redis():
-    r = redis.Redis(host=settings.redis_host, port=settings.redis_port)
+    r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
     print(f"Ping successful: {await r.ping()}")
     await r.aclose()
 
