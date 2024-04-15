7139f4a3aa94317da25e4f1d91080e32d84f6f4e
SergeKryv <krivosheevsrg@gmail.com>
2024-04-15 19:22:04 +0300

    removed comment

--- app/config.py
+++ app/config.py
@@ -4,19 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# class Settings(BaseSettings):
-#     uvicorn_host: str = Field(..., env='UVICORN_HOST')
-#     uvicorn_port: int = Field(..., env='UVICORN_PORT')
-#     uvicorn_restart: bool = Field(..., env='UVICORN_RESTART')
-#     db_name: str = Field(..., env='DB_NAME')
-#     db_user: str = Field(..., env='DB_USER')
-#     db_password: str = Field(..., env='DB_PASSWORD')
-#     db_host: str = Field(..., env='DB_HOST')
-#     db_port: int = Field(..., env='DB_PORT')
-#     redis_host: str = Field(..., env='REDIS_HOST')
-#     redis_port: int = Field(..., env='REDIS_PORT')
-
-
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
